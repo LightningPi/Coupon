bcd8e3e34d04be860220a5652d4e29fd06e1292b
LightningPi <91781766+LightningPi@users.noreply.github.com>
2022-04-11 04:14:42 +0800

    收藏優惠券功能(cherish_coupon.html專用)

--- app_php/cherish_coupon.php
+++ app_php/cherish_coupon.php
@@ -7,17 +7,17 @@ mysql_select_db("coupon");
 mysql_query("SET NAMES 'utf8'");
 
 
-$member_id=$_GET['mid'];
+$member_id=$_GET['mid'];	//會員主鍵，變數$member_id，接收由cherish_coupon.html傳送的mid
 
-$cherish_id=$_GET['cid'];
+$cherish_id=$_GET['cid'];	//優惠券主鍵，變數$cherish_id，接收由cherish_coupon.html傳送的cid
 
-$sql="INSERT INTO collect(class,coupon_id,member_id) VALUES ('1','".$cherish_id."','".$member_id."')";
+$sql="INSERT INTO collect(class,coupon_id,member_id) VALUES ('1','".$cherish_id."','".$member_id."')";  //收藏優惠功能，將優惠券主鍵$cherish_id、會員主鍵$member_id，寫入收藏資料表collect
 
 mysql_query($sql);  //寫入資料庫
 
-	$msg[]=array("result" => "已收藏優惠券");	//把 「錯誤訊息」 設定成「陣列變數」。
+	$msg[]=array("result" => "已收藏優惠券");	//把 「已收藏優惠券訊息」 設定成「陣列變數」。
 
-	echo $_GET['jsoncallback'].'('.json_encode($msg).');';  //$_GET裡的jsoncallback會在login.html裡用到。$msg的資料是php array，透過json_encode轉換成json可傳送的值。
+	echo $_GET['jsoncallback'].'('.json_encode($msg).');';  //$_GET裡的jsoncallback會在cherish_coupon.html裡用到。$msg的資料是php array，透過json_encode轉換成json可傳送的值。
 
 
 ?>
