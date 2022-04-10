2ba5b0ae57a796db829492be4bd69d9a053b6090
LightningPi <91781766+LightningPi@users.noreply.github.com>
2022-04-11 05:27:50 +0800

    問題回報功能(report.html專用)

--- app_php/report.php
+++ app_php/report.php
@@ -7,25 +7,25 @@ mysql_select_db("coupon");
 mysql_query("SET NAMES 'utf8'");
 
 
-$report_name=$_GET['report_name'];
+$report_name=$_GET['report_name'];	//稱呼，變數$report_name，接收由report.html傳送的report_name
 
-$report_email=$_GET['report_email'];
+$report_email=$_GET['report_email'];	//信箱，變數$report_email，接收由report.html傳送的report_email
 
-$report_title=$_GET['report_title'];
+$report_title=$_GET['report_title'];	//標題，變數$report_title，接收由report.html傳送的report_title
 
-$report_content=$_GET['report_content'];
+$report_content=$_GET['report_content'];	//內容，變數$report_content，接收由report.html傳送的report_content
 
 
 //echo "帳號：".$ac."密碼：".$pw;
 
 
-$sql="INSERT INTO report(name,title,content,email,type) VALUES ('".$report_name."','".$report_title."','".$report_content."','".$report_email."','2')";
+$sql="INSERT INTO report(name,title,content,email,type) VALUES ('".$report_name."','".$report_title."','".$report_content."','".$report_email."','2')";  //問題回報功能(type = 2)，將稱呼、標題、內容、信箱，寫入回報資料表report
 
 mysql_query($sql)or die("Query error:".mysql_error());  //設定$result為mysql的傳送語法，並將$sql中的SELECT語法代入
 
-	$reportdone[]=array("reportgood" => "回報成功");	//把 「錯誤訊息」 設定成「陣列變數」。
+	$reportdone[]=array("reportgood" => "回報成功");	//把 「回報成功訊息」 設定成「陣列變數」。
 
-	echo $_GET['jsoncallback'].'('.json_encode($reportdone).');';  //$_GET裡的jsoncallback會在login.html裡用到。$wrong的資料是php array，透過json_encode轉換成json可傳送的值。
+	echo $_GET['jsoncallback'].'('.json_encode($reportdone).');';  //$_GET裡的jsoncallback會在report.html裡用到。$reportdone的資料是php array，透過json_encode轉換成json可傳送的值。
 
 
 ?>
