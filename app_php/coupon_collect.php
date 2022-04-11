f1f7767a99497171d3e122d8a3a5bfde0b5d634f
LightningPi <91781766+LightningPi@users.noreply.github.com>
2022-04-11 17:25:11 +0800

    集券冊(couponcollect.html專用)

--- app_php/coupon_collect.php
+++ app_php/coupon_collect.php
@@ -7,10 +7,12 @@ mysql_select_db("coupon");
 mysql_query("SET NAMES 'utf8'");
 
 
-$member_id=$_GET['mid'];
+$member_id=$_GET['mid'];	//會員主鍵，變數$member_id，接收由couponcollect.html傳送的mid
 
 
 $sql="SELECT coupon.coupon_id,coupon.store_id,store.name,coupon.title,coupon.content,coupon.timestart,coupon.timeend,coupon.pic FROM ((collect INNER JOIN coupon ON collect.coupon_id=coupon.coupon_id) INNER JOIN store ON coupon.store_id=store.store_id) WHERE collect.member_id=".$member_id." && collect.class=1";
+
+//從收藏資料表collect，撈取會員的優惠券收藏紀錄(class=1)
 
 $result=mysql_query($sql)or die("Query error:".mysql_error());  //設定$result為mysql的傳送語法，並將$sql中的SELECT語法代入
 
@@ -22,6 +24,6 @@ while($row=mysql_fetch_assoc($result)){ //傳送mysql語法從資料庫抓值，
 
 	}
 
-echo $_GET['jsoncallback'].'('.json_encode($record).');';  //$_GET裡的jsoncallback會在index_sql_view.html裡用到。$record的資料是php array，透過json_encode轉換成json可傳送的值。
+echo $_GET['jsoncallback'].'('.json_encode($record).');';  //$_GET裡的jsoncallback會在couponcollect.html裡用到。$record的資料是php array，透過json_encode轉換成json可傳送的值。
 
 ?>
