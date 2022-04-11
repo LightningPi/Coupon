4b9253461bd37f277efe2ffb2b6ae91ceba7c1e2
LightningPi <91781766+LightningPi@users.noreply.github.com>
2022-04-11 09:12:08 +0800

    今天吃什麼功能(eat_result.html專用)

--- app_php/eat_random_test.php
+++ app_php/eat_random_test.php
@@ -6,7 +6,9 @@ mysql_connect("localhost","root","1234") or die ("Could not connect:".mysql_erro
 mysql_select_db("coupon");
 mysql_query("SET NAMES 'utf8'");
 
-$sql="SELECT coupon.coupon_id,coupon.store_id,store.name,coupon.title,coupon.content,coupon.timestart,coupon.timeend,coupon.pic,store.time,store.tel FROM coupon CROSS JOIN store WHERE coupon.store_id=store.store_id ORDER BY RAND() LIMIT 1"; //資料庫中隨機撈1筆資料
+$sql="SELECT coupon.coupon_id,coupon.store_id,store.name,coupon.title,coupon.content,coupon.timestart,coupon.timeend,coupon.pic,store.time,store.tel FROM coupon CROSS JOIN store WHERE coupon.store_id=store.store_id ORDER BY RAND() LIMIT 1"; 
+
+  //從優惠券資料表coupon，隨機撈取一筆優惠券
 
 $result=mysql_query($sql)or die("Query error:".mysql_error());  //設定$result為mysql的傳送語法，並將$sql中的SELECT語法代入
 
@@ -18,5 +20,5 @@ while($row=mysql_fetch_assoc($result)){ //傳送mysql語法從資料庫抓值，
 
 	}
 
-echo $_GET['jsoncallback'].'('.json_encode($record).');';  //$_GET裡的jsoncallback會在index_sql_view.html裡用到。$record的資料是php array，透過json_encode轉換成json可傳送的值。
+echo $_GET['jsoncallback'].'('.json_encode($record).');';  //$_GET裡的jsoncallback會在eat_result.html裡用到。$record的資料是php array，透過json_encode轉換成json可傳送的值。
 ?>
